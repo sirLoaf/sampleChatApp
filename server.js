f207aa2711562866c69dc13939a8bc8c53416f5d
Fabio Laib <flaib@luware.com>
2016-10-06 13:25:46 +0200

    small changes on config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 ,   app = express()
 ,   server = require('http').createServer(app)
 ,   io = require('socket.io').listen(server)
-,   conf = require('./config.json');
+,   conf = require('./config/config.js');
 
 server.listen(conf.port);
 
