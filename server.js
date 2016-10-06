23760af426c7d5487703b5fbe61675aa36604223
Fabio Laib <flaib@luware.com>
2016-10-06 12:53:46 +0200

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+,   app = express()
+,   server = require('http').createServer(app)
+,   io = require('socket.io').listen(server)
+,   conf = require('./config.json');
+
+server.listen(conf.port);
+
+app.configure(function(){
+	// statische Dateien ausliefern
+	app.use(express.static(__dirname + '/public'));
+});
+// wenn der Pfad / aufgerufen wird
+app.get('/', function (req, res) {
+	// so wird die Datei index.html ausgegeben
+	res.sendfile(__dirname + '/public/index.html');
+});
+// Websocket
+io.sockets.on('connection', function (socket) {
+	// der Client ist verbunden
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
+	// wenn ein Benutzer einen Text senden
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
+	});
+});
+
+// Portnummer in die Konsole schreiben
+console.log('Der Server läuft nun unter' + conf.port + '/');
